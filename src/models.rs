777e20201fda5209d13d7a807e3c7f083a155a03
austr <84636135+authagia@users.noreply.github.com>
2025-09-21 15:21:27 +0900

    add Config struct

--- src/models.rs
+++ src/models.rs
@@ -3,3 +3,6 @@ pub mod player_queue;
 
 pub mod ytdlp_worker;
 pub mod track_info;
+
+pub mod config;
+pub mod config_dev_default;
---
+++ src/models/config.rs
@@ -0,0 +1,9 @@
+use std::path::PathBuf;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Config{
+    pub data_dir: PathBuf,
+    pub ytdlp_path: String, // for Command
+} 
