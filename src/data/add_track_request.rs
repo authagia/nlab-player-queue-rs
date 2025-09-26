02b57a7d89f1d8c079ce0f6a1ab75ede86c0ed94
austr <84636135+authagia@users.noreply.github.com>
2025-09-26 23:48:16 +0900

    temporary

---
+++ src/data/add_track_request.rs
@@ -0,0 +1,3 @@
+pub struct AddTrackRequest {
+    pub url: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod services;
+mod models;
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", std::env::current_exe().unwrap().as_path().to_str().unwrap());
 }
